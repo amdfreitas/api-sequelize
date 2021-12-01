51ded357125db9ad4b0e50c4dedffe909c063383
amdfreitas <andersonmfreitas25@gmail.com>
2021-11-30 23:07:10 -0300

    resolvendo bug

--- src/controller/criaTabelas.js
+++ src/controller/criaTabelas.js
@@ -5,8 +5,8 @@ module.exports = (app) => {
         criatabelas: () => {
             const tables = [app.db.model_DB.tbforneced,app.db.model_DB.tbprodut];
 
-            tables.forEach((itens) => {
-                itens.tab().sync();
+            tables.forEach((iten) => {
+                iten.tab().sync();
             });
 
         }
